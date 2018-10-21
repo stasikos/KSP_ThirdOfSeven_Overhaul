ebc6d617d62de00784a14c51b6092ae875aa4760
Stanislav Kogut <stasikos@gmail.com>
2018-10-21 13:57:45 +0300

    One simple script to scan tech tree
    allows me to find what is missing, mistakes and mistypes

---
+++ find_missing_nodes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Scans ModuleManager cache for TechRequired = values missing in ModuleManager.TechTree
+# (quite simple and messy, slow script, don't repeat at home)
+
+SRC="../ModuleManager.ConfigCache"
+#TECH_TREE="../ModuleManager.TechTree"
+TECH_TREE="000_TechTree/TechTree.cfg"
+
+TECH_LIST=$(grep TechRequired "$SRC" | sort -u | awk '{print $3}')
+
+#echo $TECH_LIST
+for I in $TECH_LIST; do 
+	grep -q "$I" "$TECH_TREE" || echo "$I is missing"
+done
+
