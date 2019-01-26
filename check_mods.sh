0b24fca468ec6074be798c961e123f82c342c409
Stanislav Kogut <stasikos@gmail.com>
2019-01-26 12:59:18 +0200

    This is script I was dreaming for

--- check_mods.sh
+++ check_mods.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-REQUIRED="Squad zzz_ThirdOfSeven"
-REQ_OPTIONAL="SquadExpansion"
-CORE="FerramAerospaceResearch ModularFlightIntegrator ModuleManager.3.1.3.dll REPOSoftTech/BackgroundResources ThunderAerospace"
+REQUIRED=$(cat Lists/required.txt)
+REQ_OPTIONAL=$(cat Lists/req_optional.txt)
+OPTIONAL=$(cat Lists/optional.txt)
+CORE=$(cat Lists/core.txt)
+VISUAL=$(cat Lists/visual.txt)
 
 
 cd ..
@@ -15,14 +17,33 @@ done
 
 for I in $REQ_OPTIONAL ; do
        if [ ! -e "$I" ] ; then
-	       echo "Required $I is missing"
+	       echo "Optionally required $I is missing"
        fi
 done
 
 for I in $CORE ; do
        if [ ! -e "$I" ] ; then
-	       echo "Required $I is missing"
+	       echo "Core $I is missing"
        fi
+done
+
+for I in $OPTIONAL ; do
+       if [ ! -e "$I" ] ; then
+	       echo "Optional $I is missing"
+       fi
+done
+
+for I in $(ls -1) ; do
+        FOUND=0
+	for J in $REQUIRED $REQ_OPTIONAL $OPTIONAL $CORE $VISUAL; do
+		if [ "$I" = "$J" ] ; then 
+			FOUND=1
+		fi
+	done
+
+	if [ "$FOUND" -ne "1" ]; then 
+		echo "$I is not documented properly"
+	fi
 done
 
 
