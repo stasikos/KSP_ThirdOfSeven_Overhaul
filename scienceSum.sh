07503feb20784e92c75f339daf45a26b09be5a80
Stanislav Kogut <stasikos@gmail.com>
2019-02-19 22:20:16 +0200

    Fixing bugs in science nerfs

--- scienceSum.sh
+++ scienceSum.sh
@@ -8,3 +8,14 @@ grep "cost = " 000_TechTree/TechTree.cfg | awk '{sum += $3} END {print sum}'
 echo -n "Nodes: "
 grep -c "cost = " 000_TechTree/TechTree.cfg
 
+echo -n "Science points T1:"
+#grep "cost = " ../ModuleManager.TechTree | awk '{sum += $3} END {print sum}'
+grep "cost = " 000_TechTree/TechTree.cfg | awk '{if ($3<100) {sum += $3}} END {print sum}'
+
+echo -n "Science points T2:"
+#grep "cost = " ../ModuleManager.TechTree | awk '{sum += $3} END {print sum}'
+grep "cost = " 000_TechTree/TechTree.cfg | awk '{if ($3>100 && $3<500) {sum += $3}} END {print sum}'
+
+echo -n "Science points T3:"
+#grep "cost = " ../ModuleManager.TechTree | awk '{sum += $3} END {print sum}'
+grep "cost = " 000_TechTree/TechTree.cfg | awk '{if($3>500) {sum += $3}} END {print sum}'
